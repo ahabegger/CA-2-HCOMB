f235d88f66530f629db489e3f7ec3510264e38c5
habeggaj <habeggaj@miamioh.edu>
2023-12-30 15:28:48 -0500

    Create Testing for NDimLattice.py

--- NDimLattice/NDimLattice.py
+++ NDimLattice/NDimLattice.py
@@ -4,3 +4,6 @@ import numpy as np
 
 def create_n_dimensional_lattice(pdb_code):
     return None
+
+if __name__ == "__main__":
+    print("This is a module for creating N-Dimensional Lattice Structures")
