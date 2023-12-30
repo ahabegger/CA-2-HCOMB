0e0297c9965c21fc0d7d757a0b6222e3edb8342c
habeggaj <habeggaj@miamioh.edu>
2023-12-30 15:01:02 -0500

    Added Testing Infustructure

--- main.py
+++ main.py
@@ -5,17 +5,21 @@ from ThreeDimCubic.ThreeDimCubic import create_three_dim_cubic
 from Tetrahederal.Tetrahederal import create_tetrahedral_lattice
 from NDimLattice.NDimLattice import create_n_dimensional_lattice
 
+is_test = True
+structure = "1"
+pdb_code = "1A0M"
 
-print("Sample PDB Codes are 1A0M, 1A1M, 1A2M, 1A3M, 1A4M, 1A5M, 1A6M, 1A7M, 1A8M, 1A9M, 1B0M, 1B1M, 1B2M, 1B3M, 1B4M")
-pdb_code = input("Input 4-Letter PDB Code: ")
+if not is_test:
+    print("Sample PDB Codes are 1A0M, 1A1M, 1A2M, 1A3M, 1A4M, 1A5M, 1A6M, 1A7M, 1A8M, 1A9M, 1B0M, 1B1M, 1B2M, 1B3M, 1B4M")
+    pdb_code = input("Input 4-Letter PDB Code: ")
 
-print("Structure Options:")
-print("1 = CA Backbone Structure")
-print("2 = 1-Dimensional Cubic Lattice Structure")
-print("3 = 3-Dimensional Cubic Lattice Structure")
-print("4 = Tetrahedral Lattice Structure")
-print("5 = N-Dimensional Lattice Structure")
-structure = input("Input Structure: ")
+    print("Structure Options:")
+    print("1 = CA Backbone Structure")
+    print("2 = 1-Dimensional Cubic Lattice Structure")
+    print("3 = 3-Dimensional Cubic Lattice Structure")
+    print("4 = Tetrahedral Lattice Structure")
+    print("5 = N-Dimensional Lattice Structure")
+    structure = input("Input Structure: ")
 
 if structure == "1":
     xyz = create_backbone(pdb_code)
@@ -40,3 +44,4 @@ elif structure == "5":
 else:
     print("Invalid Structure")
     exit()
+
