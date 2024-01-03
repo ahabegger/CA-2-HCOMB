e23585af97aca0462e555fbbfa7d9adcf8ff0a05
habeggaj <habeggaj@miamioh.edu>
2024-01-03 01:04:30 -0500

    Created cost_calculations function for movements

--- OneDimCubic/OneDimCubic.py
+++ OneDimCubic/OneDimCubic.py
@@ -6,44 +6,23 @@ from PDB2Backbone import create_backbone
 def create_one_dim_cubic(pdb_code):
     backbone_xyz = create_backbone(pdb_code)
 
-    return backbone_xyz
+    num_rows = backbone_xyz.shape[0]
+    cost_df = pd.DataFrame(0, index=range(num_rows - 1), columns=range(6))
 
+    # For each row in the backbone_xyz DataFrame
+    for i in range(num_rows - 1):
+        costs = move_priority(backbone_xyz.iloc[i], backbone_xyz.iloc[i + 1])
+        print(costs)
 
-def convert_xyz(input_xyz):
-    # Preparing Input
-    input_df = pd.DataFrame(input_xyz)
-    input_df.columns = ['x', 'y', 'z']
-    num_rows, num_columns = input_df.shape
-
-    output_df = pd.DataFrame(-1000, index=range(num_rows), columns=input_df.columns)
-    priority_df = pd.DataFrame(0, index=range(num_rows - 1), columns=range(6))
-
-    for origin in range(num_rows - 1):
-        destination = origin + 1
-        priority = move_priority(input_df.iloc[origin], input_df.iloc[destination])
-        priority_df.iloc[origin] = priority
-
-    # Determine the First Point
-    output_df.iloc[0] = np.floor(input_df.iloc[0] - (input_df.iloc[0] % 3)).astype(int)
+    print(cost_df)
 
     movements = {
         1: [1, 0, 0], 2: [-1, 0, 0], 3: [0, 1, 0],
         4: [0, -1, 0], 5: [0, 0, 1], 6: [0, 0, -1]
     }
 
-    print(priority_df)
-
-    print(is_valid(output_df))
-
-    # Determine Next Point
-    for index, row in input_df.iterrows():
-
-        if not is_valid(output_df):
-            break
-
-    print("Correction needed")
-
-    return output_df
+    output_xyz = backbone_xyz
+    return output_xyz
 
 
 def is_valid(xyz):
@@ -53,16 +32,20 @@ def is_valid(xyz):
     return rows_are_unique
 
 
-def move_priority(input_origin, input_destination):
+def cost_calculations(input_origin, input_destination):
     moves_dict = {
-        1: input_origin.x - input_destination.x,
-        2: input_destination.x - input_origin.x,
-        3: input_origin.y - input_destination.y,
-        4: input_destination.y - input_origin.y,
-        5: input_origin.z - input_destination.z,
-        6: input_destination.z - input_origin.z
+        1: input_origin.X - input_destination.X,
+        2: input_destination.X - input_origin.X,
+        3: input_origin.Y - input_destination.Y,
+        4: input_destination.Y - input_origin.Y,
+        5: input_origin.Z - input_destination.Z,
+        6: input_destination.Z - input_origin.Z
     }
-    return sorted(moves_dict, key=moves_dict.get)
+    lowest_cost = min(moves_dict.values())
+    for key in moves_dict.keys():
+        moves_dict[key] += abs(lowest_cost)
+
+    return moves_dict
 
 
 if __name__ == "__main__":
