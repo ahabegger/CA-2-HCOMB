144e1f373a18dc0aae4d370120db20b071ea87e0
habeggaj <habeggaj@miamioh.edu>
2024-01-03 19:43:09 -0500

    Deleting N-Dim using Platonic Solids instead

--- NDimLattice/NDimLattice.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-import numpy as np
-
-
-def create_n_dimensional_lattice(pdb_code):
-    return None
-
-if __name__ == "__main__":
-    print("This is a module for creating N-Dimensional Lattice Structures")
