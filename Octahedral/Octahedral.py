bc62ab024f6bbd4671d50468e514dd6b5b11fbec
habeggaj <habeggaj@miamioh.edu>
2024-01-04 02:46:12 -0500

    Setting up Greedy Algo call with variable

--- Octahedral/Octahedral.py
+++ Octahedral/Octahedral.py
@@ -4,6 +4,7 @@ import pandas as pd
 from PDB2Backbone import create_backbone
 import Octahedral.XYZ_helper as xyz_helper
 import Visualization as plot
+from Octahedral.Greedy import greedy_octahedral
 
 '''
 Octahedral.py
@@ -15,24 +16,22 @@ def create_octahedral(pdb_code):
     backbone_xyz = create_backbone(pdb_code)
 
     num_rows = backbone_xyz.shape[0]
-    cost_df = pd.DataFrame(0.0, index=range(num_rows - 1), columns=range(1, 7))
+    cost_df = pd.DataFrame(0.0, index=range(num_rows - 1), columns=range(1, 9))
 
     # For each row in the backbone_xyz DataFrame
     for i in range(num_rows - 1):
         costs = cost_calculations(backbone_xyz.iloc[i], backbone_xyz.iloc[i + 1])
         cost_df.iloc[i] = costs
 
-    print(cost_df)
+    normalize_cost_df = normalize_cost(cost_df)
+    initial_moves = [1] * (num_rows - 1)
 
-    # Find the lowest cost for each row
-    lowest_cost = cost_df.idxmin(axis=1)
-    lowest_cost = lowest_cost.tolist()
+    moves, cost = greedy_octahedral(initial_moves, normalize_cost_df)
 
-    lowest_xyz = xyz_helper.convert_to_xyz(lowest_cost)
+    xyz = xyz_helper.convert_to_xyz(moves)
+    plot.visualize(xyz, backbone_xyz, title="Octahedral (8 Move) Lattice")
 
-    plot.visualize(lowest_xyz, backbone_xyz, title="Octahedral (8 Move) Lattice")
-
-    return lowest_xyz
+    return xyz
 
 
 def cost_calculations(input_origin, input_destination):
@@ -55,3 +54,15 @@ def cost_calculations(input_origin, input_destination):
     }
 
     return move_cost
+
+
+def normalize_cost(costs):
+    normalize_costs = costs.copy()
+    num_rows = costs.shape[0]
+
+    for i in range(num_rows):
+        lowest_cost = min(costs.iloc[i])
+        for col in costs.columns:
+            normalize_costs.at[i, col] -= abs(lowest_cost)
+
+    return normalize_costs
