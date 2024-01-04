cc4f5ffce3442a54491164e2c5137a84d7351c7b
habeggaj <habeggaj@miamioh.edu>
2024-01-03 22:04:48 -0500

    Create convert to xyz function for Icosahedral

--- Icosahedral/XYZ_helper.py
+++ Icosahedral/XYZ_helper.py
@@ -1,15 +1,22 @@
+import math
 import pandas as pd
 
 
-def covert_to_xyz(moves_6):
-    xyz = pd.DataFrame(0.0, index=range(len(moves_6) + 1), columns=['X', 'Y', 'Z'])
+def covert_to_xyz(moves_20):
+    xyz = pd.DataFrame(0.0, index=range(len(moves_20) + 1), columns=['X', 'Y', 'Z'])
+    phi = (1 + math.sqrt(5)) / 2
+
     movements = {
-        1: [1, 0, 0], 2: [-1, 0, 0], 3: [0, 1, 0],
-        4: [0, -1, 0], 5: [0, 0, 1], 6: [0, 0, -1]
+        1: normalize([0, 1, phi]), 2: normalize([0, -1, phi]),
+        3: normalize([0, 1, -phi]), 4: normalize([0, -1, -phi]),
+        5: normalize([1, phi, 0]), 6: normalize([-1, phi, 0]),
+        7: normalize([1, -phi, 0]), 8: normalize([-1, -phi, 0]),
+        9: normalize([phi, 0, 1]), 10: normalize([phi, 0, -1]),
+        11: normalize([-phi, 0, 1]), 12: normalize([-phi, 0, -1])
     }
 
-    for i in range(len(moves_6)):
-        xyz.iloc[i + 1] = xyz.iloc[i] + movements[moves_6[i]]
+    for i in range(len(moves_20)):
+        xyz.iloc[i + 1] = xyz.iloc[i] + movements[moves_20[i]]
 
     return xyz
 
@@ -19,3 +26,8 @@ def is_valid(xyz):
     copy = copy[~(copy == -1000).any(axis=1)]
     rows_are_unique = copy.duplicated().sum() == 0
     return rows_are_unique
+
+
+def normalize(vector):
+    magnitude = math.sqrt(sum([x ** 2 for x in vector]))
+    return [x / magnitude for x in vector]
