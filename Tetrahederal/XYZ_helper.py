6a90432c8e4d4f25f6643beb7d7b54ebcbc06761
habeggaj <habeggaj@miamioh.edu>
2024-01-03 19:18:01 -0500

    Convert to XYZ for Tetrahederal

--- Tetrahederal/XYZ_helper.py
+++ Tetrahederal/XYZ_helper.py
@@ -1,2 +1,24 @@
-def covert_to_xyz(lowest_cost):
-    return None
+import math
+import pandas as pd
+
+
+def covert_to_xyz(moves_4):
+    xyz = pd.DataFrame(0.0, index=range(len(moves_4) + 1), columns=['X', 'Y', 'Z'])
+    movements = {
+        1: [1/math.sqrt(3), 1/math.sqrt(3), 1/math.sqrt(3)],
+        2: [-1/math.sqrt(3), -1/math.sqrt(3), 1/math.sqrt(3)],
+        3: [-1/math.sqrt(3), 1/math.sqrt(3), -1/math.sqrt(3)],
+        4: [1/math.sqrt(3), -1/math.sqrt(3), -1/math.sqrt(3)]
+    }
+
+    for i in range(len(moves_4)):
+        xyz.iloc[i + 1] = xyz.iloc[i] + movements[moves_4[i]]
+
+    return xyz
+
+
+def is_valid(xyz):
+    copy = xyz
+    copy = copy[~(copy == -1000).any(axis=1)]
+    rows_are_unique = copy.duplicated().sum() == 0
+    return rows_are_unique
