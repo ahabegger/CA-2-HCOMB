3fc3ae39f15da76c26ea6db9254d4421e914057f
habeggaj <habeggaj@miamioh.edu>
2024-01-03 21:31:10 -0500

    Convert XYZ for Dodecahedral

--- Dodecahedral/XYZ_helper.py
+++ Dodecahedral/XYZ_helper.py
@@ -1,11 +1,20 @@
+import math
 import pandas as pd
 
 
 def covert_to_xyz(moves_6):
     xyz = pd.DataFrame(0.0, index=range(len(moves_6) + 1), columns=['X', 'Y', 'Z'])
+    phi = (1 + math.sqrt(5)) / 2
+
     movements = {
-        1: [1, 0, 0], 2: [-1, 0, 0], 3: [0, 1, 0],
-        4: [0, -1, 0], 5: [0, 0, 1], 6: [0, 0, -1]
+        1: normalize([1, 1, 1]), 2: normalize([1, 1, -1]), 3: normalize([1, -1, 1]), 4: normalize([1, -1, -1]),
+        5: normalize([-1, 1, 1]), 6: normalize([-1, 1, -1]), 7: normalize([-1, -1, 1]), 8: normalize([-1, -1, -1]),
+        9: normalize([0, 1 / phi, phi]), 10: normalize([0, -1 / phi, phi]), 11: normalize([0, 1 / phi, -phi]),
+        12: normalize([0, -1 / phi, -phi]),
+        13: normalize([1 / phi, phi, 0]), 14: normalize([-1 / phi, phi, 0]), 15: normalize([1 / phi, -phi, 0]),
+        16: normalize([-1 / phi, -phi, 0]),
+        17: normalize([phi, 0, 1 / phi]), 18: normalize([-phi, 0, 1 / phi]), 19: normalize([phi, 0, -1 / phi]),
+        20: normalize([-phi, 0, -1 / phi])
     }
 
     for i in range(len(moves_6)):
@@ -19,3 +28,8 @@ def is_valid(xyz):
     copy = copy[~(copy == -1000).any(axis=1)]
     rows_are_unique = copy.duplicated().sum() == 0
     return rows_are_unique
+
+
+def normalize(vector):
+    magnitude = math.sqrt(sum([x ** 2 for x in vector]))
+    return [x / magnitude for x in vector]
