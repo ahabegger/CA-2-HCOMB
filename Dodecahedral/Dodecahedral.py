35e74449536b6405c607a3f9b7bab3a2032b5fd2
habeggaj <habeggaj@miamioh.edu>
2024-01-05 00:03:10 -0500

    Preparing the inputs for greedy algorithm

--- Dodecahedral/Dodecahedral.py
+++ Dodecahedral/Dodecahedral.py
@@ -1,5 +1,6 @@
 import numpy as np
 import pandas as pd
+from Dodecahedral.Greedy import greedy_dodecahederal
 from PDB2Backbone import create_backbone
 import Dodecahedral.XYZ_helper as xyz_helper
 import Visualization as plot
@@ -15,7 +16,7 @@ def create_dodecahedral(pdb_code):
     backbone_xyz = create_backbone(pdb_code)
 
     num_rows = backbone_xyz.shape[0]
-    cost_df = pd.DataFrame(0.0, index=range(num_rows - 1), columns=range(1, 7))
+    cost_df = pd.DataFrame(0.0, index=range(num_rows - 1), columns=range(1, 21))
 
     # For each row in the backbone_xyz DataFrame
     for i in range(num_rows - 1):
@@ -24,15 +25,15 @@ def create_dodecahedral(pdb_code):
 
     print(cost_df)
 
-    # Find the lowest cost for each row
-    lowest_cost = cost_df.idxmin(axis=1)
-    lowest_cost = lowest_cost.tolist()
+    normalize_cost_df = normalize_cost(cost_df)
+    initial_moves = [1] * (num_rows - 1)
 
-    lowest_xyz = xyz_helper.convert_to_xyz(lowest_cost)
+    moves, cost = greedy_dodecahederal(initial_moves, normalize_cost_df)
 
-    plot.visualize(lowest_xyz, backbone_xyz, title="Dodecahedral (20 Move) Lattice")
+    xyz = xyz_helper.convert_to_xyz(moves)
+    plot.visualize(xyz, backbone_xyz, title="Dodecahedral (20 Move) Lattice")
 
-    return lowest_xyz
+    return xyz
 
 
 def cost_calculations(input_origin, input_destination):
@@ -74,3 +75,15 @@ def cost_calculations(input_origin, input_destination):
 def normalize(vector):
     magnitude = np.linalg.norm(vector)
     return vector / magnitude
+
+
+def normalize_cost(costs):
+    normalize_costs = costs.copy()
+    num_rows = costs.shape[0]
+
+    for i in range(num_rows):
+        lowest_cost = min(costs.iloc[i])
+        for col in costs.columns:
+            normalize_costs.at[i, col] -= abs(lowest_cost)
+
+    return normalize_costs
