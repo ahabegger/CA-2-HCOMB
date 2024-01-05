6815b5e3bd610d8ad74df6025701c1533edf134a
habeggaj <habeggaj@miamioh.edu>
2024-01-05 03:01:18 -0500

    Generalizing the XYZ helper to work for all Structures

--- XYZ_helper.py
+++ XYZ_helper.py
@@ -1,13 +1,25 @@
 import numpy as np
 import pandas as pd
-import math
 
 
-def convert_to_xyz(moves_8):
+def convert_to_xyz(moves, possible_movements):
+    # Pre-allocate space for xyz coordinates
+    xyz = np.zeros((len(moves) + 1, 3))
+
+    # Calculate the cumulative sum of movements
+    for i, move in enumerate(moves):
+        xyz[i + 1] = xyz[i] + possible_movements[move - 1]  # -1 because moves are 1-indexed
+
+    return pd.DataFrame(xyz, columns=['X', 'Y', 'Z'])
+
+
+def is_valid(xyz):
+    return xyz.duplicated().sum() == 0
+
+'''    
+# Define movements as a list of vectors
     # Calculate the normalization factor
     norm_factor = 1 / math.sqrt(3)
-
-    # Define movements as a list of vectors
     movements = [
         [norm_factor, norm_factor, norm_factor],
         [-norm_factor, norm_factor, norm_factor],
@@ -18,16 +30,4 @@ def convert_to_xyz(moves_8):
         [-norm_factor, norm_factor, -norm_factor],
         [-norm_factor, -norm_factor, -norm_factor]
     ]
-
-    # Pre-allocate space for xyz coordinates
-    xyz = np.zeros((len(moves_8) + 1, 3))
-
-    # Calculate the cumulative sum of movements
-    for i, move in enumerate(moves_8):
-        xyz[i + 1] = xyz[i] + movements[move - 1]  # -1 because moves are 1-indexed
-
-    return pd.DataFrame(xyz, columns=['X', 'Y', 'Z'])
-
-
-def is_valid(xyz):
-    return xyz.duplicated().sum() == 0
+    '''
