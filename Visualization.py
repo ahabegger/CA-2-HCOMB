46ba525a28384bf75b355febd585b0551ae52c89
habeggaj <habeggaj@miamioh.edu>
2024-01-12 13:34:33 -0500

    Adding Visualization for Honeycomb display

--- Visualization.py
+++ Visualization.py
@@ -1,34 +1,43 @@
 # Import Outside Libraries
-import matplotlib.pyplot as plt
 import pandas as pd
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
+import numpy as np
 
 
-def visualize(xyz, title='Protein Structure'):
+def visualize(xyz, title='Protein Structure', connections=True):
     # Convert to DataFrame for easier processing
-    df = pd.DataFrame(xyz)
-
-    # Identify unique and repeated points
-    is_repeated = df.duplicated(subset=['X', 'Y', 'Z'], keep=False)
+    df = pd.DataFrame(xyz, columns=['X', 'Y', 'Z'])
 
     # Create 3D plot
     fig = plt.figure()
     fig.suptitle(title, fontsize=16)
     ax = fig.add_subplot(projection='3d')
 
-    # Plot all points as a line plot to maintain the original order
-    ax.plot3D(df['X'], df['Y'], df['Z'], color='blue', label='Line Connection')
+    # Plot points
+    ax.scatter(df['X'], df['Y'], df['Z'], color='green', label='Points')
 
-    # Overplot unique points in green and repeated points in red
-    ax.scatter(df.loc[~is_repeated, 'X'], df.loc[~is_repeated, 'Y'], df.loc[~is_repeated, 'Z'],
-               color='green',
-               label='Unique Points')
-    ax.scatter(df.loc[is_repeated, 'X'], df.loc[is_repeated, 'Y'], df.loc[is_repeated, 'Z'],
-               color='red',
-               label='Repeated Points')
+    # Draw connections
+    if connections:
+        ax.plot3D(df['X'], df['Y'], df['Z'], color='blue', label='Line Connection')
+    else:
+        tolerance = 0.1
+        for i in range(len(df)):
+            for j in range(i + 1, len(df)):
+                # Calculate the distance between points
+                point1 = df.iloc[i]
+                point2 = df.iloc[j]
+                distance = np.linalg.norm(point1 - point2)
+
+                # Draw a line if the distance is close to 1 (within the specified tolerance)
+                if abs(distance - 1) <= tolerance:
+                    ax.plot([point1['X'], point2['X']],
+                            [point1['Y'], point2['Y']],
+                            [point1['Z'], point2['Z']],
+                            color='blue', linewidth=0.5)
 
     # Show plot
     plt.legend()
     plt.show()
 
     return None
-
