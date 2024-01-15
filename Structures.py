6e5e967eec4e2839aaef164309dae7887359f2b1
habeggaj <habeggaj@miamioh.edu>
2024-01-15 05:00:59 -0500

    Moving Movement functionality to Structures.py

--- Structures.py
+++ Structures.py
@@ -1,7 +1,7 @@
 from Greedy import greedy_lattice
 from PDB2Backbone import create_backbone
-import Movements
 import Tilt as tilt
+import numpy as np
 
 '''
 Structures.py
@@ -14,28 +14,53 @@ def create_lattice(num_moves, pdb_code):
     backbone_xyz = create_backbone(pdb_code)
 
     # Create Movements
-    movements = Movements.movements(num_moves)
+    movements = get_movements(num_moves)
 
     # Optimize Movements with Tilt.py
-    optimized_movements, cost_df = tilt.optimize_tilt(backbone_xyz, movements)
+    optimized_movements, cost_matrix, structure_cost = tilt.optimize_tilt(backbone_xyz, movements)
     print(f"Optimized Movements: {optimized_movements}")
-    structure_cost = cost_df.min(axis=1).sum()
     print(f"Structure Cost: {structure_cost}")
 
-    # Fit Movements to Lattice
-    normalize_cost_df = normalize_cost(cost_df)
-    xyz, fitted_cost, time = greedy_lattice(normalize_cost_df, optimized_movements)
+    xyz, fitted_cost, time = greedy_lattice(cost_matrix, optimized_movements)
 
     return xyz, structure_cost + fitted_cost, time
 
 
-def normalize_cost(costs):
-    normalize_costs = costs.copy()
-    num_rows = costs.shape[0]
+def get_movements(num_moves):
+    # Define Math Calculations
+    sqrt_3_div_2 = np.sqrt(3) / 2
+    sqrt_2_div_2 = np.sqrt(2) / 2
 
-    for i in range(num_rows):
-        lowest_cost = min(costs.iloc[i])
-        for col in costs.columns:
-            normalize_costs.at[i, col] -= abs(lowest_cost)
-
-    return normalize_costs
+    if num_moves == 4:
+        # SQUARE TILING (4 MOVES)
+        return [
+            [1, 0, 0],
+            [-1, 0, 0],
+            [0, 1, 0],
+            [0, -1, 0]
+        ]
+    elif num_moves == 6:
+        # CUBIC HONEYCOMB (6 MOVES)
+        return [
+            [1, 0, 0], [-1, 0, 0],
+            [0, 1, 0], [0, -1, 0],
+            [0, 0, 1], [0, 0, -1]
+        ]
+    elif num_moves == 8:
+        # TRIANGULAR PRISMATIC HONEYCOMB (8 MOVES)
+        return [
+            [sqrt_3_div_2, 0.5, 0], [-sqrt_3_div_2, 0.5, 0],
+            [0, 1, 0], [sqrt_3_div_2, -0.5, 0],
+            [-sqrt_3_div_2, -0.5, 0], [0, -1, 0],
+            [0, 0, 1], [0, 0, -1]
+        ]
+    elif num_moves == 12:
+        # TETRAHEDRAL-OCTAHEDRAL HONEYCOMB (12 MOVES)
+        return [
+            [sqrt_2_div_2, sqrt_2_div_2, 0], [sqrt_2_div_2, 0, sqrt_2_div_2],
+            [0, sqrt_2_div_2, sqrt_2_div_2], [-sqrt_2_div_2, -sqrt_2_div_2, 0],
+            [-sqrt_2_div_2, 0, -sqrt_2_div_2], [0, -sqrt_2_div_2, -sqrt_2_div_2],
+            [sqrt_2_div_2, -sqrt_2_div_2, 0], [sqrt_2_div_2, 0, -sqrt_2_div_2],
+            [0, sqrt_2_div_2, -sqrt_2_div_2], [-sqrt_2_div_2, sqrt_2_div_2, 0],
+            [-sqrt_2_div_2, 0, sqrt_2_div_2], [0, -sqrt_2_div_2, sqrt_2_div_2]
+        ]
