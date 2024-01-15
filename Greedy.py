31429c60ebbb5d003e958a2d4618771b97b4ed1f
habeggaj <habeggaj@miamioh.edu>
2024-01-15 06:21:41 -0500

    Changing is_valid and convert xyz to work with numpy

--- Greedy.py
+++ Greedy.py
@@ -13,13 +13,12 @@ def greedy_lattice(cost_matrix, movements):
     # Executes the greedy lattice instance 10 times and returns the best result
     best_moves = []
     best_cost = float('inf')
-    start_time = time.time()
 
     for i in range(5):
         initial_moves = [(i % cost_matrix.shape[1])] * cost_matrix.shape[0]
 
         # Run the greedy lattice instance
-        moves, final_cost = greedy_lattice_instance(initial_moves, cost_matrix, movements)
+        moves, final_cost = greedy_lattice_instance(i, initial_moves, cost_matrix, movements)
 
         # If the final cost is better than the current best cost, update the best moves and best cost
         if final_cost < best_cost:
@@ -28,18 +27,17 @@ def greedy_lattice(cost_matrix, movements):
         if best_cost == 0:
             break
 
-    return convert_to_xyz(best_moves, movements), best_cost, time.time() - start_time
+    fitted_xyz = pd.DataFrame(convert_to_xyz(best_moves, movements),
+                              columns=['X', 'Y', 'Z'])
+
+    return fitted_xyz, best_cost
 
 
-def greedy_lattice_instance(moves, cost_matrix, movements):
+def greedy_lattice_instance(test_num, moves, cost_matrix, movements):
     start_time = time.time()
 
-    print('-' * 50)
-    print(f"TESTING: {len(movements)} - MOVE LATTICE")
-    print(f"Initial Moves: {moves}")
-    print(f"Initial Cost: {get_cost(moves, cost_matrix)}")
-    print('-' * 50)
-
+    print(f"Test #{test_num}: ", end='')
+    print(f"Cost={format(get_cost(moves, cost_matrix), '.2f')}", end='|')
     refined_moves = moves.copy()
 
     # Refine the moves using local search
@@ -49,20 +47,20 @@ def greedy_lattice_instance(moves, cost_matrix, movements):
 
     final_cost = get_cost(refined_moves, cost_matrix)
     elapsed_time = time.time() - start_time
-    print(f"--- Final Moves: {refined_moves}")
-    print(f"--- {elapsed_time} seconds")
-    print(f"--- {final_cost} cost")
+    print(f"\nResults #{test_num}: {format(final_cost, '.3f')} in {int(elapsed_time)} seconds")
 
     return refined_moves, final_cost
 
 
-def test_battery(test_num, num_tests, moves, cost_matrix, movements):
-    lowest_cost = get_cost(moves, cost_matrix)
+def test_battery(window_size, num_tests, moves, cost_matrix, movements):
+    starting_cost = get_cost(moves, cost_matrix)
+    lowest_cost = starting_cost
     lowest_moves = moves.copy()
 
+    print(f'LR{window_size}', end='=')
+
     for i in range(num_tests):
-        print(f"Test {i + 1} for {test_num}")
-        refined_moves = run_refinement(test_num, moves, cost_matrix, movements)
+        refined_moves = run_refinement(window_size, moves, cost_matrix, movements)
         refined_cost = get_cost(refined_moves, cost_matrix)
         if refined_cost == 0:
             return refined_moves
@@ -70,10 +68,12 @@ def test_battery(test_num, num_tests, moves, cost_matrix, movements):
             lowest_cost = get_cost(refined_moves, cost_matrix)
             lowest_moves = refined_moves.copy()
 
+    print(f"{format(lowest_cost - starting_cost, '.2f')}", end='|')
+
     return lowest_moves
 
 
-def run_refinement(test_num, input_moves, cost_matrix, movements):
+def run_refinement(window_size, input_moves, cost_matrix, movements):
     cost = get_cost(input_moves, cost_matrix)
     if cost == 0:
         return input_moves
@@ -82,36 +82,30 @@ def run_refinement(test_num, input_moves, cost_matrix, movements):
     output_moves = input_moves.copy()
 
     while change_cost != 0:
-        start_time = time.time()
-        print(f'Local Search Refinement {test_num}')
-        output_moves = local_search_refinement(test_num, output_moves, cost_matrix, movements)
+        output_moves = local_search_refinement(window_size, output_moves, cost_matrix, movements)
         change_cost = get_cost(output_moves, cost_matrix) - cost
-        print(f"Cost Change: {change_cost}")
-        print(f"Cost: {get_cost(output_moves, cost_matrix)}")
-        print(f"Total Time: {time.time() - start_time}")
-        print('-' * 50)
         cost = get_cost(output_moves, cost_matrix)
 
     return output_moves
 
 
-def local_search_refinement(num, moves, cost_matrix, movements):
-    indices = np.array(range(len(moves) - num + 1))
+def local_search_refinement(window, moves, cost_matrix, movements):
+    indices = np.array(range(len(moves) - window + 1))
     randomized_indices = np.random.permutation(indices)
 
     for i in randomized_indices:
-        total_cost = sum(cost_matrix[i + offset, moves[i + offset]] for offset in range(num))
+        total_cost = sum(cost_matrix[i + offset, moves[i + offset]] for offset in range(window))
 
         if total_cost == 0:
             continue
 
-        cost_rows = [cost_matrix[i + offset] for offset in range(num)]
+        cost_rows = [cost_matrix[i + offset] for offset in range(window)]
         combinations = find_combinations(cost_rows, total_cost=total_cost, movements=movements)
 
         for combo in combinations:
             new_moves = moves.copy()
-            new_moves[i: i + num] = combo
-            if is_valid(convert_to_xyz(new_moves, movements)):
+            new_moves[i: i + window] = combo
+            if is_valid_moves(new_moves, movements):
                 moves = new_moves
                 break
 
@@ -123,7 +117,7 @@ def find_combinations(cost_rows, total_cost, movements):
     def find_combinations_recursive(row, current_cost, current_indices):
         if row == len(cost_rows):
             # Check if the combination is valid and within the cost limit
-            if current_cost < total_cost and is_valid(convert_to_xyz(current_indices, movements)):
+            if current_cost < total_cost and is_valid_moves(current_indices, movements):
                 combinations.append((tuple(current_indices), current_cost))
             return
 
@@ -142,8 +136,10 @@ def get_cost(moves, cost_matrix):
     return np.sum(cost_matrix[np.arange(len(moves)), moves])
 
 
-def is_valid(xyz):
-    return not xyz.duplicated().any()
+def is_valid_moves(moves, possible_movements):
+    xyz = convert_to_xyz(moves, possible_movements)
+    _, unique_indices = np.unique(xyz, axis=0, return_index=True)
+    return len(unique_indices) == xyz.shape[0]
 
 
 def convert_to_xyz(moves, possible_movements):
@@ -160,5 +156,4 @@ def convert_to_xyz(moves, possible_movements):
     # Efficiently compute the cumulative sum of movements
     xyz[1:] = np.cumsum(possible_movements[moves], axis=0)
 
-    return pd.DataFrame(xyz, columns=['X', 'Y', 'Z'])
-
+    return xyz
