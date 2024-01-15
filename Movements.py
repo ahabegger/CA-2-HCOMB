ff727239b046bd1139d81b319b4dcc1a83a8855a
habeggaj <habeggaj@miamioh.edu>
2024-01-15 04:46:09 -0500

    Removing normalization function

--- Movements.py
+++ Movements.py
@@ -1,5 +1,3 @@
-# Import Outside Libraries
-import math
 import numpy as np
 
 '''
@@ -16,42 +14,33 @@ def movements(num_moves):
     if num_moves == 4:
         # SQUARE TILING (4 MOVES)
         return [
-            normalize([1, 0, 0]),
-            normalize([-1, 0, 0]),
-            normalize([0, 1, 0]),
-            normalize([0, -1, 0])
+            [1, 0, 0],
+            [-1, 0, 0],
+            [0, 1, 0],
+            [0, -1, 0]
         ]
     elif num_moves == 6:
         # CUBIC HONEYCOMB (6 MOVES)
         return [
-            normalize([1, 0, 0]), normalize([-1, 0, 0]),
-            normalize([0, 1, 0]), normalize([0, -1, 0]),
-            normalize([0, 0, 1]), normalize([0, 0, -1])
+            [1, 0, 0], [-1, 0, 0],
+            [0, 1, 0], [0, -1, 0],
+            [0, 0, 1], [0, 0, -1]
         ]
     elif num_moves == 8:
         # TRIANGULAR PRISMATIC HONEYCOMB (8 MOVES)
         return [
-            normalize([sqrt_3_div_2, 0.5, 0]), normalize([-sqrt_3_div_2, 0.5, 0]),
-            normalize([0, 1, 0]), normalize([sqrt_3_div_2, -0.5, 0]),
-            normalize([-sqrt_3_div_2, -0.5, 0]), normalize([0, -1, 0]),
-            normalize([0, 0, 1]), normalize([0, 0, -1])
+            [sqrt_3_div_2, 0.5, 0], [-sqrt_3_div_2, 0.5, 0],
+            [0, 1, 0], [sqrt_3_div_2, -0.5, 0],
+            [-sqrt_3_div_2, -0.5, 0], [0, -1, 0],
+            [0, 0, 1], [0, 0, -1]
         ]
     elif num_moves == 12:
         # TETRAHEDRAL-OCTAHEDRAL HONEYCOMB (12 MOVES)
         return [
-            normalize([sqrt_2_div_2, sqrt_2_div_2, 0]), normalize([sqrt_2_div_2, 0, sqrt_2_div_2]),
-            normalize([0, sqrt_2_div_2, sqrt_2_div_2]), normalize([-sqrt_2_div_2, -sqrt_2_div_2, 0]),
-            normalize([-sqrt_2_div_2, 0, -sqrt_2_div_2]), normalize([0, -sqrt_2_div_2, -sqrt_2_div_2]),
-            normalize([sqrt_2_div_2, -sqrt_2_div_2, 0]), normalize([sqrt_2_div_2, 0, -sqrt_2_div_2]),
-            normalize([0, sqrt_2_div_2, -sqrt_2_div_2]), normalize([-sqrt_2_div_2, sqrt_2_div_2, 0]),
-            normalize([-sqrt_2_div_2, 0, sqrt_2_div_2]), normalize([0, -sqrt_2_div_2, sqrt_2_div_2])
+            [sqrt_2_div_2, sqrt_2_div_2, 0], [sqrt_2_div_2, 0, sqrt_2_div_2],
+            [0, sqrt_2_div_2, sqrt_2_div_2], [-sqrt_2_div_2, -sqrt_2_div_2, 0],
+            [-sqrt_2_div_2, 0, -sqrt_2_div_2], [0, -sqrt_2_div_2, -sqrt_2_div_2],
+            [sqrt_2_div_2, -sqrt_2_div_2, 0], [sqrt_2_div_2, 0, -sqrt_2_div_2],
+            [0, sqrt_2_div_2, -sqrt_2_div_2], [-sqrt_2_div_2, sqrt_2_div_2, 0],
+            [-sqrt_2_div_2, 0, sqrt_2_div_2], [0, -sqrt_2_div_2, sqrt_2_div_2]
         ]
-
-
-def normalize(vector):
-    try:
-        magnitude = np.linalg.norm(vector)
-        return vector / magnitude
-    except:
-        magnitude = math.sqrt(sum([x ** 2 for x in vector]))
-        return [x / magnitude for x in vector]
