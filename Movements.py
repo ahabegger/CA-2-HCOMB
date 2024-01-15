649444b01a5169e17a9cb59dcc3e4c47d34663d2
habeggaj <habeggaj@miamioh.edu>
2024-01-15 00:31:45 -0500

    Correcting the movements to create Uniform Convex Honeycombs

--- Movements.py
+++ Movements.py
@@ -2,7 +2,6 @@
 import math
 import numpy as np
 
-
 '''
 Movements.py
 This Script is as a library of movements for the Structures.py script.
@@ -10,55 +9,42 @@ This Script is as a library of movements for the Structures.py script.
 
 
 def movements(num_moves):
-    # Define Math Constants
-    phi = (1 + math.sqrt(5)) / 2
+    # Define Math Calculations
+    sqrt_3_div_2 = np.sqrt(3) / 2
+    sqrt_2_div_2 = np.sqrt(2) / 2
 
-    # Define movements as a list of vectors
     if num_moves == 4:
+        # SQUARE TILING (4 MOVES)
         return [
-            normalize([1, 1, 1]),
-            normalize([-1, -1, 1]),
-            normalize([-1, 1, -1]),
-            normalize([1, -1, -1])
+            normalize([1, 0, 0]),
+            normalize([-1, 0, 0]),
+            normalize([0, 1, 0]),
+            normalize([0, -1, 0])
         ]
     elif num_moves == 6:
+        # CUBIC HONEYCOMB (6 MOVES)
         return [
             normalize([1, 0, 0]), normalize([-1, 0, 0]),
             normalize([0, 1, 0]), normalize([0, -1, 0]),
             normalize([0, 0, 1]), normalize([0, 0, -1])
         ]
     elif num_moves == 8:
+        # TRIANGULAR PRISMATIC HONEYCOMB (8 MOVES)
         return [
-            normalize([1, 1, 1]),
-            normalize([-1, 1, 1]),
-            normalize([1, -1, 1]),
-            normalize([1, 1, -1]),
-            normalize([-1, -1, 1]),
-            normalize([1, -1, -1]),
-            normalize([-1, 1, -1]),
-            normalize([-1, -1, -1])
+            normalize([sqrt_3_div_2, 0.5, 0]), normalize([-sqrt_3_div_2, 0.5, 0]),
+            normalize([0, 1, 0]), normalize([sqrt_3_div_2, -0.5, 0]),
+            normalize([-sqrt_3_div_2, -0.5, 0]), normalize([0, -1, 0]),
+            normalize([0, 0, 1]), normalize([0, 0, -1])
         ]
     elif num_moves == 12:
+        # TETRAHEDRAL-OCTAHEDRAL HONEYCOMB (12 MOVES)
         return [
-            normalize([0, 1, phi]), normalize([0, -1, phi]),
-            normalize([0, 1, -phi]), normalize([0, -1, -phi]),
-            normalize([1, phi, 0]), normalize([-1, phi, 0]),
-            normalize([1, -phi, 0]), normalize([-1, -phi, 0]),
-            normalize([phi, 0, 1]), normalize([phi, 0, -1]),
-            normalize([-phi, 0, 1]), normalize([-phi, 0, -1])
-        ]
-    elif num_moves == 20:
-        return [
-            normalize([1, 1, 1]), normalize([1, 1, -1]),
-            normalize([1, -1, 1]), normalize([1, -1, -1]),
-            normalize([-1, 1, 1]), normalize([-1, 1, -1]),
-            normalize([-1, -1, 1]), normalize([-1, -1, -1]),
-            normalize([0, 1 / phi, phi]), normalize([0, -1 / phi, phi]),
-            normalize([0, 1 / phi, -phi]), normalize([0, -1 / phi, -phi]),
-            normalize([1 / phi, phi, 0]), normalize([-1 / phi, phi, 0]),
-            normalize([1 / phi, -phi, 0]), normalize([-1 / phi, -phi, 0]),
-            normalize([phi, 0, 1 / phi]), normalize([-phi, 0, 1 / phi]),
-            normalize([phi, 0, -1 / phi]), normalize([-phi, 0, -1 / phi])
+            normalize([sqrt_2_div_2, sqrt_2_div_2, 0]), normalize([sqrt_2_div_2, 0, sqrt_2_div_2]),
+            normalize([0, sqrt_2_div_2, sqrt_2_div_2]), normalize([-sqrt_2_div_2, -sqrt_2_div_2, 0]),
+            normalize([-sqrt_2_div_2, 0, -sqrt_2_div_2]), normalize([0, -sqrt_2_div_2, -sqrt_2_div_2]),
+            normalize([sqrt_2_div_2, -sqrt_2_div_2, 0]), normalize([sqrt_2_div_2, 0, -sqrt_2_div_2]),
+            normalize([0, sqrt_2_div_2, -sqrt_2_div_2]), normalize([-sqrt_2_div_2, sqrt_2_div_2, 0]),
+            normalize([-sqrt_2_div_2, 0, sqrt_2_div_2]), normalize([0, -sqrt_2_div_2, sqrt_2_div_2])
         ]
 
 
