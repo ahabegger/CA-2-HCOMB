aec6a385dc8ca5f426016aa8b930bd205a1232ae
habeggaj <habeggaj@miamioh.edu>
2024-01-16 01:52:34 -0500

    Add table of XYZ and plots for Report

--- TransformationReports/GenerateReport.py
+++ TransformationReports/GenerateReport.py
@@ -1,7 +1,9 @@
-# Import Outside Libraries
+import pandas as pd
 import requests
 import nglview
 import os
+from matplotlib import pyplot as plt
+from PDB2Backbone import create_backbone
 
 '''
 GenerateReport.py
@@ -22,10 +24,10 @@ def create_report(pdb_code, output_xyz, structure):
     create_diagram(f"TransformationReports/PDB_Files/{pdb_code}_modified.pdb")
 
     # Create the Report
-    write_report_html(pdb_code, structure)
+    write_report_html(pdb_code, structure, output_xyz)
 
 
-def write_report_html(pdb_code, structure):
+def write_report_html(pdb_code, structure, xyz):
     input_diagram = ""
     with open(f"{pdb_code}_diagram.html") as file:
         for line in file:
@@ -40,6 +42,8 @@ def write_report_html(pdb_code, structure):
     output_diagram = output_diagram.split("body>")[1][:-2]
     os.remove(f"{pdb_code}_modified_diagram.html")
 
+    pdb_url = f'https://files.rcsb.org/download/{pdb_code}.pdb'
+
     top = f"<!DOCTYPE html>\n" \
           f"<html lang=\"en\">\n" \
           f"<head>\n" \
@@ -51,18 +55,41 @@ def write_report_html(pdb_code, structure):
           f"</head>\n" \
           f"<body>\n" \
           f"<h1>PDB2LatticePy Report</h1>\n" \
-          f"<h2>{pdb_code} Changed to {structure}</h2>\n" \
+          f"<h2>{pdb_code} Changed to {structure} Structure</h2>\n" \
           f"<h3>Input Model</h3>\n" \
-          f"TransformationReports/PDB_Files/{pdb_code}.pdb\n"
+          f"TransformationReports/PDB_Files/{pdb_code}.pdb\n" \
+          f"\nDownload Here : <a href=\"{pdb_url}\">{pdb_code}.pdb</a>\n"
 
     bottom = '</body>\n<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" ' \
              'integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" ' \
              'crossorigin="anonymous"></script>\n</html> '
 
+    intermediate = f'<h3>Intermediate Model</h3>\n'
+
+    backbone = create_backbone(pdb_code)
+    backbone_plot = plot_structure_to_image(backbone[['X', 'Y', 'Z']],
+                                            f"TransformationReports/Reports/Plots/{pdb_code}_backbone.png",
+                                            title="CA Backbone Structure")
+
     middle = f'<h3>Output Model</h3>\n' \
              f'TransformationReports/PDB_Files/{pdb_code}_modified.pdb\n'
 
-    html = top + input_diagram + middle + output_diagram + bottom
+    output_plot = plot_structure_to_image(xyz,
+                                          f"TransformationReports/Reports/Plots/{pdb_code}_{structure}.png",
+                                          title=f"{structure} Structure")
+    output_diagram = output_plot + output_diagram
+
+    compare = f'<h3>Comparison XYZ</h3>\n'
+    backbone = start_at_zero(backbone)
+    backbone = backbone.rename(columns={'X': 'Backbone_X', 'Y': 'Backbone_Y', 'Z': 'Backbone_Z'})
+    xyz = xyz.rename(columns={'X': f'{structure}_X', 'Y': f'{structure}_Y', 'Z': f'{structure}_Z'})
+    xyz = xyz.drop(columns=['ID', 'Amino Acid'])
+    combined_df = pd.concat([backbone, xyz], axis=1)
+    combined_table = combined_df.to_html()
+
+    html = (top + input_diagram + intermediate + backbone_plot + middle +
+            output_diagram + compare + combined_table + bottom)
+
     with open(f"TransformationReports/Reports/{pdb_code}_transformation.html", 'w') as file:
         file.write(html)
 
@@ -135,5 +162,34 @@ def create_modified_pdb(pdb_code, output_xyz, structure):
         file.write(end)
 
 
-if __name__ == "__main__":
-    pass
+def plot_structure_to_image(xyz, filename, title='Protein Structure'):
+    # Convert to DataFrame for easier processing
+    df = pd.DataFrame(xyz, columns=['X', 'Y', 'Z'])
+
+    # Create 3D plot
+    fig = plt.figure()
+    fig.suptitle(title, fontsize=16)
+    ax = fig.add_subplot(projection='3d')
+
+    # Plot points
+    ax.scatter(df['X'], df['Y'], df['Z'], color='green', label='Points')
+
+    # Draw connections
+    ax.plot3D(df['X'], df['Y'], df['Z'], color='blue', label='Line Connection')
+
+    # Save the figure as a PNG file
+    plt.savefig(filename, bbox_inches='tight')
+
+    plt.close(fig)
+
+    reference_filename = "Plots/" + filename.split('/')[-1]
+
+    return f"<img src=\"{reference_filename}\" alt=\"{title}\">\n"
+
+
+def start_at_zero(df):
+    for col in ['X', 'Y', 'Z']:
+        if col in df.columns:
+            original_first_value = df[col].iloc[0]
+            df[col] = df[col] - original_first_value
+    return df
