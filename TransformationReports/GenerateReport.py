38e6a5aff8901bd5407efc096c18dfa3fbd7345e
habeggaj <habeggaj@miamioh.edu>
2024-01-16 02:26:30 -0500

    Adding XYZ, Adding CA Backbone, Abstracting other Generate Files

--- TransformationReports/GenerateReport.py
+++ TransformationReports/GenerateReport.py
@@ -1,9 +1,7 @@
 import pandas as pd
-import requests
-import nglview
-import os
-from matplotlib import pyplot as plt
 from PDB2Backbone import create_backbone
+import TransformationReports.GenerateDiagram as diagrams
+import TransformationReports.GeneratePDB as pdb
 
 '''
 GenerateReport.py
@@ -14,33 +12,24 @@ report containing the original PDB file, the modified PDB file, and the diagrams
 
 def create_report(pdb_code, output_xyz, structure):
     # Download the PDB file
-    download_pdb(pdb_code)
+    pdb.download_pdb(pdb_code)
 
     # Create the Modified PDB File
-    create_modified_pdb(pdb_code, output_xyz, structure)
+    pdb.create_modified_pdb(pdb_code, output_xyz, structure)
 
     # Create the Diagrams
-    create_diagram(f"TransformationReports/PDB_Files/{pdb_code}.pdb")
-    create_diagram(f"TransformationReports/PDB_Files/{pdb_code}_modified.pdb")
+    diagrams.create_nglview(f"TransformationReports/PDB_Files/{pdb_code}.pdb")
+    diagrams.create_nglview(f"TransformationReports/PDB_Files/{pdb_code}_modified.pdb")
 
     # Create the Report
     write_report_html(pdb_code, structure, output_xyz)
 
 
 def write_report_html(pdb_code, structure, xyz):
-    input_diagram = ""
-    with open(f"{pdb_code}_diagram.html") as file:
-        for line in file:
-            input_diagram += line
-    input_diagram = input_diagram.split("body>")[1][:-2]
-    os.remove(f"{pdb_code}_diagram.html")
-
-    output_diagram = ""
-    with open(f"{pdb_code}_modified_diagram.html") as file:
-        for line in file:
-            output_diagram += line
-    output_diagram = output_diagram.split("body>")[1][:-2]
-    os.remove(f"{pdb_code}_modified_diagram.html")
+    input_diagram_file = f"{pdb_code}_diagram.html"
+    output_diagram_file = f"{pdb_code}_modified_diagram.html"
+    input_diagram = diagrams.get_nglview_html(input_diagram_file)
+    output_diagram = diagrams.get_nglview_html(output_diagram_file)
 
     pdb_url = f'https://files.rcsb.org/download/{pdb_code}.pdb'
 
@@ -57,8 +46,8 @@ def write_report_html(pdb_code, structure, xyz):
           f"<h1>PDB2LatticePy Report</h1>\n" \
           f"<h2>{pdb_code} Changed to {structure} Structure</h2>\n" \
           f"<h3>Input Model</h3>\n" \
-          f"TransformationReports/PDB_Files/{pdb_code}.pdb\n" \
-          f"\nDownload Here : <a href=\"{pdb_url}\">{pdb_code}.pdb</a>\n"
+          f"<p>TransformationReports/PDB_Files/{pdb_code}.pdb</p>\n" \
+          f"<p>Download Here : <a href=\"{pdb_url}\">{pdb_code}.pdb</a></p>\n"
 
     bottom = '</body>\n<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" ' \
              'integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" ' \
@@ -67,16 +56,18 @@ def write_report_html(pdb_code, structure, xyz):
     intermediate = f'<h3>Intermediate Model</h3>\n'
 
     backbone = create_backbone(pdb_code)
-    backbone_plot = plot_structure_to_image(backbone[['X', 'Y', 'Z']],
-                                            f"TransformationReports/Reports/Plots/{pdb_code}_backbone.png",
-                                            title="CA Backbone Structure")
+    plot_filename = f"TransformationReports/Reports/Plots/{pdb_code}_backbone.png"
+    backbone_plot = diagrams.plot_structure_to_image(backbone[['X', 'Y', 'Z']],
+                                                     plot_filename,
+                                                     title="CA Backbone Structure")
 
     middle = f'<h3>Output Model</h3>\n' \
-             f'TransformationReports/PDB_Files/{pdb_code}_modified.pdb\n'
+             f'<p>TransformationReports/PDB_Files/{pdb_code}_modified.pdb</p>\n'
 
-    output_plot = plot_structure_to_image(xyz,
-                                          f"TransformationReports/Reports/Plots/{pdb_code}_{structure}.png",
-                                          title=f"{structure} Structure")
+    plot_filename = f"TransformationReports/Reports/Plots/{pdb_code}_{structure.replace(' ', '_')}.png"
+    output_plot = diagrams.plot_structure_to_image(xyz,
+                                                   plot_filename,
+                                                   title=f"{structure} Structure")
     output_diagram = output_plot + output_diagram
 
     compare = f'<h3>Comparison XYZ</h3>\n'
@@ -90,7 +81,7 @@ def write_report_html(pdb_code, structure, xyz):
     html = (top + input_diagram + intermediate + backbone_plot + middle +
             output_diagram + compare + combined_table + bottom)
 
-    with open(f"TransformationReports/Reports/{pdb_code}_transformation.html", 'w') as file:
+    with open(f"TransformationReports/Reports/{pdb_code}_{structure.replace(' ', '_')}.html", 'w') as file:
         file.write(html)
 
 
