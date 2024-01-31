272b54df1cd80e5277dfcb648cd882e88b81ad47
habeggaj <habeggaj@miamioh.edu>
2024-01-30 22:21:26 -0500

    Create RMSD Metric

--- Performance.py
+++ Performance.py
@@ -1,11 +1,34 @@
-'''
+"""
 Performance.py
-'''
+"""
+
+import numpy as np
 
 
 def calculate_tm_align():
     tm_score = 0
     return tm_score
 
-def calculate_rmsd():
-    return 0
+
+def calculate_rmsd(template_ca_xyz, target_ca_xyz):
+    """
+    Calculate the Root-Mean-Square Deviation (RMSD) between two sets of points.
+
+    :param template_ca_xyz: Numpy array of xyz coordinates for the template
+    :param target_ca_xyz: Numpy array of xyz coordinates for the target
+    :return: rmsd value
+    """
+    if template_ca_xyz.shape != target_ca_xyz.shape:
+        raise ValueError("The input arrays must have the same shape.")
+
+    # Calculate the squared differences
+    diff_squared = np.square(template_ca_xyz - target_ca_xyz)
+
+    # Compute the mean of the squared differences
+    mean_diff_squared = np.mean(diff_squared)
+
+    # Calculate the square root of the mean
+    rmsd = np.sqrt(mean_diff_squared)
+
+    return rmsd
+
